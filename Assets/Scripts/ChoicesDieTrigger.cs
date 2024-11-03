79659de0aad34a95e27d1891bac81e29091f64d7
EsaPutraStudents <everready.esa@students.amikom.ac.id>
2024-11-03 11:33:43 +0700

    Damagable Player

--- Assets/Scripts/ChoicesDieTrigger.cs
+++ Assets/Scripts/ChoicesDieTrigger.cs
@@ -8,20 +8,40 @@ public class ChoicesDieTrigger : MonoBehaviour
     [SerializeField] private int health = 90;
     public Slider healthBar;    
     public GameObject panel;
+    private bool isInvincible = false;
+    private float timeSinceHit = 0;
+    public float invincibleTime = 1f;
 
     void Update()
     {
         healthBar.value = health;
+        if (isInvincible)
+        {
+            if(timeSinceHit > invincibleTime)
+            {
+                isInvincible = false;
+                timeSinceHit = 0;
+            }
+            else
+            {
+                timeSinceHit += Time.deltaTime;
+            }
+        }
     }
+
+    
 
     public void TakeDamage(int damageAmount)
     {
-        health -= damageAmount;
-
+        if (!isInvincible) { 
+            health -= damageAmount;
+            isInvincible = true;
+        }
         if (health <= 0)
         {
             Die();
-        }        
+        }
+        
     }
 
     private void Die()
